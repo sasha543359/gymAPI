574566d940176db248033b7c30b1b5db65cc94dc
Alejandro <sasha543359@gmail.com>
2023-04-14 00:45:08 +0300

    I added GymController in controller folder and i create a Moldels folder and i added Customer and CustomerSubscription classes

---
+++ gymAPI/gymAPI/Models/Customer.cs
@@ -0,0 +1,34 @@
+﻿namespace gymAPI.Models
+{
+    public enum Gender
+    {
+        Male,
+        Female,
+        Transgender
+    }
+
+    public class Customer
+    {
+
+        // включая его имя, фамилию, дату рождения, пол и другую персональную информацию.
+        public string FirstName { get; set; } 
+        public string LastName { get; set; }
+
+        public CustomerSubscription Subscription { get; set; }
+        public Gender Gender { get; set; } = Gender.Male;
+        public int Age { get; set; }
+        public DateTime BirthDay { get; set; } = DateTime.Now;
+
+        public Customer(string FirstName, string LastName, int Age, CustomerSubscription subscription)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Age = Age;
+            Subscription = subscription;
+        }
+
+
+
+
+    }
+}
---
+++ gymAPI/gymAPI/Models/CustomerSubscription.cs
@@ -0,0 +1,52 @@
+﻿namespace gymAPI.Models
+{
+    public enum Subscription
+    {
+        Basic = 0 ,
+        Silver = 10,
+        Gold = 15
+    }
+
+    public class CustomerSubscription
+    {
+        
+        public Subscription Subscription { get; set; }
+        public int Price { get; set; }
+
+        public DateTime PurschaseDay { get; set; } = DateTime.Now;
+
+        public DateTime ExpirationDay { get; set; }
+
+        public CustomerSubscription(Subscription subscription)
+        {
+            this.Subscription = subscription;
+            this.Price = (int)subscription;
+            PurschaseDay = DateTime.Now;
+            ExpirationDay = Expiration(PurschaseDay);
+
+        }
+
+        private DateTime Expiration(DateTime PurschaseDay)
+        {
+            PurschaseDay = DateTime.Now;
+
+            DateTime ExpirationDate;
+
+            switch (Subscription)
+            {
+                    case Subscription.Basic:
+                        return ExpirationDate = PurschaseDay.AddDays(7);
+
+                    case Subscription.Silver:
+                        return ExpirationDate = PurschaseDay.AddDays(30);
+
+                    case Subscription.Gold:
+                        return ExpirationDate = PurschaseDay.AddDays(180);
+                    
+                    default:
+                        return DateTime.Now;  
+            }
+        }
+
+    }
+}
